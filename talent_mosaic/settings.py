956887daf2818e230ffaf45ed5bb9ee55df6f1f2
turiliffiu <52448161+turiliffiu@users.noreply.github.com>
2025-12-06 21:40:31 +0100

    Update settings.py

--- talent_mosaic/settings.py
+++ talent_mosaic/settings.py
@@ -73,17 +73,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'talent_mosaic.wsgi.application'
 
 # DATABASE
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config('DB_NAME', default='talent_mosaic'),
+#        'USER': config('DB_USER', default='talent_user'),
+#        'PASSWORD': config('DB_PASSWORD'),
+#        'HOST': config('DB_HOST', default='localhost'),
+#        'PORT': config('DB_PORT', default='5432'),
+#        'CONN_MAX_AGE': 600,
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='talent_mosaic'),
-        'USER': config('DB_USER', default='talent_user'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-        'CONN_MAX_AGE': 600,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # CACHE
 CACHES = {
