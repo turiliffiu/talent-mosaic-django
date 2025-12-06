631eefc8947ff165bddf2058d76b43b78bfc700b
turiliffiu <52448161+turiliffiu@users.noreply.github.com>
2025-12-06 22:10:20 +0100

    Update settings.py

--- talent_mosaic/settings.py
+++ talent_mosaic/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 # INSTALLED APPS
 INSTALLED_APPS = [
